f388ffac7d1b93d35010b7f4f8b554844b633a35
nitroruski <antsihag@gmail.com>
2022-12-19 18:18:54 +0200

    exercise 3.4-3.6

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ const persons =
   }
 ]
 
+const generateId = () => {
+  return Math.floor(Math.random() * 99999999)
+}
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
@@ -55,6 +59,45 @@ app.get('/api/persons/:id', (request, response) => {
   }
   
   response.json(filtered)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+
+  // check if the person exists and delete the resource if so  
+  const personToDelete = persons.findIndex(person => person.id === id)
+  if (personToDelete !== -1) {
+    persons.splice(personToDelete, 1)
+  }
+
+  // return 204 regardless
+  return response.status(204)
+})
+
+app.post('/api/persons', (request, response) => {
+  const id = generateId()
+  
+  const { name, number } = request.body
+  console.log(name, number)
+
+  // check if the name or number is missing
+  if (!name || !number) {
+    return response.status(400).json({
+      error: 'name or number is missing'
+    })
+  }
+
+  // check if the name already exists
+  if (persons.find(person => person.name === name)) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  // create the new person object and add it to the data structure
+  const newPerson = { id, name, number }
+  persons.push(newPerson)
+  response.json(newPerson)
 })
 
 app.listen(PORT, () => {
